84d871c6f246de5f9c1a6b06662935c7c627ec2d
xzhou016 <xzhou016@ucr.edu>
2016-05-17 01:58:42 +0000

    added exit flag

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,31 @@
 using namespace std;
 int main(int argc, char const *argv[]) {
 
-
-  //print_host_user(); // print out login info
-
-  //parsing begin
-  string command_line;
-  getline(cin, command_line);
-
-  //tokenize input
-  Tokenizer<string> token(command_line) ;
-  token.parse(); // separate string
-  //token.print();
-  
- //execvp(token.command_line)
- //token.setVector(check_comment(token.getVector()));
-  check_comment(token.command_collection);
-  //token.print();
-
-  execute(token.command_collection,
-          token.command_collection.size(),
-          true, false , 0, 0);
-  
+  bool EXIT_FLAG = false;
+    
+  while(EXIT_FLAG){
+      //print_host_user(); // print out login info
+    
+      //parsing begin
+      string command_line;
+      getline(cin, command_line);
+      if(command_line = "exit")
+        EXIT_FLAG = true;
+    
+      //tokenize input
+      Tokenizer<string> token(command_line) ;
+      token.parse(); // separate string
+      //token.print();
+      
+     //execvp(token.command_line)
+     //token.setVector(check_comment(token.getVector()));
+      check_comment(token.command_collection);
+      //token.print();
+    
+      execute(token.command_collection,
+              token.command_collection.size(),
+              true, false , 0, 0);
+      
+  }
   return 0;
 }
