e4ef4da8c13001fea9a4bbfa654793f17c482a1d
xzhou016 <xzhou016@ucr.edu>
2016-06-08 22:09:41 -0700

    test fix

--- test/redirect.sh
+++ test/redirect.sh
@@ -6,6 +6,14 @@ A-Z < testIO/testInput
 
 ls > testIO/testOutput
 echo muh test > testIO/testOutput
+A-Z > testIO/testOutput
+echo test > testIO/testOutput
 
+ls | echo
+cat | grep
+ps | ls
+cat < testIO/testInput | tr A-Z a-z | tee testOput | tr a-z
+
+exit
 exit
 EOF
